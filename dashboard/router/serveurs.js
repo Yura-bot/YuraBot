d44ffe2f779e2c1c22cfb8e27316d991d798a9e4
root <root@dash.yurabot.xyz>
2020-12-06 10:50:13 -0500

    💻 Mise à jour du Dashboard : Alertes + cookies notice + Boutton Activation/Désactivation du système de bienvenue.

--- dashboard/router/serveurs.js
+++ dashboard/router/serveurs.js
@@ -1,56 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const CheckAuth = require('../auth/CheckAuth');
-const path = require("path");
 
 let bot = require("../../main.js")
-
-const dataDir = path.resolve(`${process.cwd()}${path.sep}dashboard`); // The absolute path of current this directory.
-const templateDir = path.resolve(`${dataDir}${path.sep}views`);
-
-const renderTemplate = (res, req, template, data = {}) => {
-
-    let guildSettingsExist = bot.guildSettings.has(`${req.params.guildID}`)
-
-    let obj = {
-        prefix: "?",
-        lang: "french",
-    }
-    let prefix;
-    let guildLanguage;
-    let language2;
-
-    if (guildSettingsExist) {
-        prefix = bot.guildSettings.get(`${req.params.guildID}`, "prefix")
-        guildLanguage = bot.guildSettings.get(`${req.params.guildID}`, "lang")
-    } else {
-        bot.guildSettings.set(`${req.params.guildID}`, obj)
-        prefix = bot.default_prefix;
-        guildLanguage = "english"
-    }
-
-    if (guildLanguage === "english") { 
-        guildLanguage = "Anglais"
-        language2 = "Français" 
-    } else  { 
-        guildLanguage = "Français"
-        language2 = "Anglais" 
-    }
-
-    const baseData = {
-        bot: bot,
-        path: req.path,
-        name: (req.isAuthenticated() ? `${req.user.username}` : `Profil`),
-        avatar: (req.isAuthenticated() ? `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png` : `https://image.noelshack.com/fichiers/2020/36/1/1598862029-disc.png`),
-        status: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : "Se connecter"),
-        user: req.isAuthenticated() ? req.user : null,
-        prefix: prefix,
-        guildLanguage: guildLanguage,
-        language2: language2
-    };
-    
-    res.render(path.resolve(`${templateDir}${path.sep}${template}`), Object.assign(baseData, data));
-};
 
 router.get("/:guildID", CheckAuth, (req, res) => {
 
@@ -76,8 +28,7 @@ router.get("/:guildID", CheckAuth, (req, res) => {
         guildLanguage = bot.guildSettings.get(`${req.params.guildID}`, "lang")
     } else {
         bot.guildSettings.set(`${req.params.guildID}`, obj)
-        prefix = bot.default_prefix;
-        guildLanguage = "english"
+        return res.redirect("/serveurs/"+req.params.guildID);
     }
 
     if (guildLanguage === "english") { 
@@ -103,13 +54,40 @@ router.get("/:guildID", CheckAuth, (req, res) => {
         messageType: "success",
         prefix: prefix,
         guildLanguage: guildLanguage,
-        language2: language2
+        language2: language2,
+        alert: false
     });
 }).post("/:guildID", CheckAuth, async function(req, res) {
 
     let guild = req.bot.guilds.cache.get(req.params.guildID);
     if (!guild) return res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${req.bot.user.id}&scope=bot&permissions=-1&guild_id=${req.params.guildID}`);
     if (!req.bot.guilds.cache.get(req.params.guildID).members.cache.get(req.user.id).hasPermission("MANAGE_GUILD")) return res.redirect("/");
+
+    let guildSettingsExist = bot.guildSettings.has(`${req.params.guildID}`)
+
+    let obj = {
+        prefix: "?",
+        lang: "french",
+    }
+    let prefix;
+    let guildLanguage;
+    let language2;
+
+    if (guildSettingsExist) {
+        prefix = bot.guildSettings.get(`${req.params.guildID}`, "prefix")
+        guildLanguage = bot.guildSettings.get(`${req.params.guildID}`, "lang")
+    } else {
+        bot.guildSettings.set(`${req.params.guildID}`, obj)
+        return res.redirect("/serveurs/"+req.params.guildID);
+    }
+
+    if (guildLanguage === "english") { 
+        guildLanguage = "Anglais"
+        language2 = "Français" 
+    } else  { 
+        guildLanguage = "Français"
+        language2 = "Anglais" 
+    }
 
     const member = guild.members.cache.get(req.user.id);
     if (!member) return res.redirect("/");
@@ -135,7 +113,24 @@ router.get("/:guildID", CheckAuth, (req, res) => {
     }
     await bot.guildSettings.set(`${req.params.guildID}`, language, "lang")
 
-    renderTemplate(res, req, "guild.ejs", { guild, alert: "Your settings have been saved." });
+    res.render("guild", {
+        name: (req.isAuthenticated() ? `${req.user.username}` : `Profil`),
+        avatar: (req.isAuthenticated() ? `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png` : `https://image.noelshack.com/fichiers/2020/36/1/1598862029-disc.png`),
+        status: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : "Se connecter"),
+        botclient: req.client.user,
+        bot: bot,
+        user: req.user,
+        login: "oui",
+        guild: guild,
+        avatarURL: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+        iconURL: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=32`,
+        message: "",
+        messageType: "success",
+        prefix: prefix,
+        guildLanguage: guildLanguage,
+        language2: language2,
+        alert: true
+    });
 
 }).get("/:guildID/tools/welcome", CheckAuth, async(req, res) => {
 
@@ -146,29 +141,19 @@ router.get("/:guildID", CheckAuth, (req, res) => {
     let guildSettingsExist = bot.guildSettings.has(`${req.params.guildID}`)
 
     if (guildSettingsExist === false ) {
-        return res.redirect("/:guildID");
+        return res.redirect("/serveurs/"+req.params.guildID);
     }
 
-    let welcomeEnabled = bot.guildSettings.has(`${req.params.guildID}`, "welcomeMessage")
-
-    let obj = {
-        welcomeChannel: "",
-        welcomeMessage: "Hello!",
-        welcomeImage: false,
-        welcomeImageURL: "",
-    }
+    let welcomeEnabled = bot.guildSettings.has(`${req.params.guildID}`, "welcomePlug")
 
     let welcomeChannel; 
     let welcomeMessage;
     let welcomeImage;
 
-    if (welcomeEnabled === false ) {
-        await bot.guildSettings.update(`${req.params.guildID}`, obj)
-        return res.redirect("/");
-    } else {
-        welcomeChannel = bot.guildSettings.get(`${req.params.guildID}`, "welcomeChannel")
-        welcomeMessage = bot.guildSettings.get(`${req.params.guildID}`, "welcomeMessage")
-        welcomeImage = bot.guildSettings.get(`${req.params.guildID}`, "welcomeImage")
+    if (welcomeEnabled) {
+        welcomeChannel = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeChannel")
+        welcomeMessage = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeMessage")
+        welcomeImage = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeImage")
     }
 
     res.render("items/welcome", {
@@ -187,7 +172,8 @@ router.get("/:guildID", CheckAuth, (req, res) => {
         welcomeChannel: welcomeChannel,
         welcomeMessage: welcomeMessage,
         welcomeEnabled: welcomeEnabled,
-        welcomeImage: welcomeImage
+        welcomeImage: welcomeImage,
+        alert: false
     });
 
     
@@ -202,22 +188,64 @@ router.get("/:guildID", CheckAuth, (req, res) => {
     if (!member) return res.redirect("/");
     if (!member.permissions.has("MANAGE_GUILD")) return res.redirect("/");
 
+    let welcomeEnabled = bot.guildSettings.has(`${req.params.guildID}`, "welcomePlug")
+
+    let welcomeChannel; 
+    let welcomeMessage;
+    let welcomeImage;
+
+    if (welcomeEnabled) {
+        welcomeChannel = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeChannel")
+        welcomeMessage = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeMessage")
+        welcomeImage = bot.guildSettings.get(`${req.params.guildID}`, "welcomePlug.welcomeImage")
+    }
+
     let data  = req.body;
 
+    //Status :
+    let statusWelcome = data.statusWelcome;
+
+    if (statusWelcome != "on") {
+        bot.guildSettings.delete(`${req.params.guildID}`, "welcomePlug")
+        return res.redirect("/serveurs/"+req.params.guildID+"/tools/welcome");
+    }
+
     // Channel : 
-    let welcomeChannel = guild.channels.cache.find((ch) => "#"+ch.name === data.channelID).id;
-    bot.guildSettings.set(`${req.params.guildID}`, welcomeChannel, "welcomeChannel")
-
+    let welcomeChannelConfig = guild.channels.cache.find((ch) => "#"+ch.name === data.channelID).id;
     //Message
-    let welcomeMessage = data.welcomeMessage;
-    bot.guildSettings.set(`${req.params.guildID}`, welcomeMessage, "welcomeMessage")
-
+    let welcomeMessageConfig = data.welcomeMessage;
     //Image ?
+    let welcomeImageConfig = data.withImage === "on"
 
-    let welcomeImage = data.withImage === "on"
-    bot.guildSettings.set(`${req.params.guildID}`, welcomeImage, "welcomeImage")
+    const obj = {
+        welcomePlug: {
+            welcomeChannel: welcomeChannelConfig,
+            welcomeMessage: welcomeMessageConfig,
+            welcomeImage: welcomeImageConfig,
+            welcomeImageURL: "URL",
+        }
+    }
+    await bot.guildSettings.update(`${req.params.guildID}`, obj)
 
-    renderTemplate(res, req, "guild.ejs", { guild, alert: "Your settings have been saved." });
+    res.render("items/welcome", {
+        name: (req.isAuthenticated() ? `${req.user.username}` : `Profil`),
+        avatar: (req.isAuthenticated() ? `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png` : `https://image.noelshack.com/fichiers/2020/36/1/1598862029-disc.png`),
+        status: (req.isAuthenticated() ? `${req.user.username}#${req.user.discriminator}` : "Se connecter"),
+        botclient: req.client.user,
+        bot: bot,
+        user: req.user,
+        login: "oui",
+        guild: guild,
+        avatarURL: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png`,
+        iconURL: `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=32`,
+        message: "",
+        messageType: "success",
+        welcomeChannel: welcomeChannel,
+        welcomeMessage: welcomeMessage,
+        welcomeEnabled: welcomeEnabled,
+        welcomeImage: welcomeImage,
+        alert: true
+    });
 
 })
 
