0135476442b427ba17188a6c49d1d48070af2c89
Pronicio (root) <root@dash.yurabot.xyz>
2020-12-19 12:22:10 -0500

    🎊  Dernières modif pour la V3

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,8 @@
 module.exports = async(client) => {
     console.log("» "+client.user.username+" est prêt, connécté en tant que "+client.user.tag+".");
     await client.dash.load(client);
+
+//Status
+let status = `yurabot.xyz | ?help | ${client.guilds.cache.size} guilds !`
+    client.user.setActivity(status, {type: "PLAYING"})
 }
