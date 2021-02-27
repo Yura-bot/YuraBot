b5f7001faf6b88c16eb57077895a2831719c18a2
Pronicio (root) <root@dash.yurabot.xyz>
2021-02-27 08:06:54 -0500

    🎰 Intégration du système de Metrics (Instatus).

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,7 @@
+const axios = require("axios")
+
 module.exports = async(client) => {
     console.log("» "+client.user.username+" est prêt, connécté en tant que "+client.user.tag+".");
-
-    if (client.shard.ids.includes(0) && !client.spawned){
-        await client.dash.load(client);
-    }
 
     await client.db.init();
 
@@ -12,4 +10,25 @@ module.exports = async(client) => {
     //Status
     let status = `yurabot.xyz | ?help | ${guildCount.reduce((acc, guildCount) => acc + guildCount, 0)} guilds !`
     client.user.setActivity(status, {type: "PLAYING"})
+
+    if (client.shard.ids.includes(0) && !client.spawned){
+        await client.dash.load(client);
+
+        setInterval(maFonction, 43200000, client);
+
+        function maFonction(client) {
+            let StatusPageAPIKey = "8873bae76c9207298069da0fd47f9af7"
+
+            let data = {
+                "timestamp": Math.round(new Date().getTime() / 1000),
+                "value": client.ws.ping
+            }
+    
+            axios.post('https://api.instatus.com/v1/ckik44c7230ah0744mfbec2hb/metrics/ckjfr2xac0hqj0775pdd3xcno', data, {
+                headers: {
+                    'Authorization': `Bearer ${StatusPageAPIKey}`,
+                }
+            });
+        }
+    }
 }
